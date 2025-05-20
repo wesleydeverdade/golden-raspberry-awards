41eeee0c555f41e53cf31f749fe9fc5b86830503
=Wesley Magalhães <=wesley.magalhaes@printi.com.br>
2025-05-20 04:41:23 -0300

    Fixing eslint command

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,18 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import { defineConfig } from "eslint/config";
-import prettier from "prettier";
-import eslintConfigPrettier from "eslint-config-prettier";
-import eslintPluginPrettier from "eslint-plugin-prettier";
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
-  { ignores: ["node_modules/", "build/", "dist/"] },
-  { files: ["**/*.{js,mjs,cjs,ts}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: globals.node } },
-  tseslint.configs.recommended,
-  prettier,
-  eslintConfigPrettier,
+  { ignores: ['node_modules/', 'build/', 'dist/'] },
   {
-    plugins: { prettier: eslintPluginPrettier },
-    rules: {
-      "prettier/prettier": "error"
-    } 
-  }
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    languageOptions: { globals: globals.node },
+  },
+  tseslint.configs.recommended,
 ]);
