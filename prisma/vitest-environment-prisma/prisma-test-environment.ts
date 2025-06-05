bb74d4b23e62e94d367c9d5162ac57d9e3f60688
Wesley Magalhães <wesleymagalhaes5@gmail.com>
2025-06-05 16:57:23 -0300

    fix | readme

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -2,110 +2,8 @@ import 'dotenv/config';
 
 import { execSync } from 'node:child_process';
 import type { Environment } from 'vitest/environments';
-import { PrismaClient } from '@prisma/client';
 
 const testDatabaseName = 'test-golden-raspberry-awards.db';
-
-const seedTestEnvironment = async (prisma: PrismaClient) => {
-  console.log('✅ Seed init.');
-
-  const csvData = [
-    {
-      year: 2008,
-      title: 'Movie 1',
-      studios: ['Studio 1'],
-      producers: ['Producer 1'],
-      winner: true,
-    },
-    {
-      year: 2009,
-      title: 'Movie 2',
-      studios: ['Studio 1'],
-      producers: ['Producer 1'],
-      winner: true,
-    },
-    {
-      year: 2018,
-      title: 'Movie 3',
-      studios: ['Studio 2'],
-      producers: ['Producer 2'],
-      winner: true,
-    },
-    {
-      year: 2019,
-      title: 'Movie 4',
-      studios: ['Studio 2'],
-      producers: ['Producer 2'],
-      winner: true,
-    },
-    {
-      year: 1900,
-      title: 'Movie 5',
-      studios: ['Studio 3'],
-      producers: ['Producer 3'],
-      winner: true,
-    },
-    {
-      year: 1999,
-      title: 'Movie 6',
-      studios: ['Studio 3'],
-      producers: ['Producer 3'],
-      winner: true,
-    },
-    {
-      year: 2000,
-      title: 'Movie 7',
-      studios: ['Studio 4'],
-      producers: ['Producer 4'],
-      winner: true,
-    },
-    {
-      year: 2099,
-      title: 'Movie 8',
-      studios: ['Studio 4'],
-      producers: ['Producer 4'],
-      winner: true,
-    },
-  ];
-
-  for (const row of csvData) {
-    const studios = await Promise.all(
-      row.studios.map(async (studio) =>
-        prisma.studio.upsert({
-          where: { name: studio },
-          update: {},
-          create: { name: studio },
-        }),
-      ),
-    );
-
-    const producers = await Promise.all(
-      row.producers.map(async (producer) =>
-        prisma.producer.upsert({
-          where: { name: producer },
-          update: {},
-          create: { name: producer },
-        }),
-      ),
-    );
-
-    await prisma.movie.create({
-      data: {
-        title: row.title,
-        year: row.year,
-        winner: row.winner,
-        studios: {
-          connect: studios.map((s) => ({ id: s.id })),
-        },
-        producers: {
-          connect: producers.map((p) => ({ id: p.id })),
-        },
-      },
-    });
-  }
-
-  console.log('✅ Seed finished.');
-};
 
 export default <Environment>{
   name: 'prisma',
@@ -118,12 +16,12 @@ export default <Environment>{
 
     console.log('✅ Database created.');
 
-    const prisma = new PrismaClient();
+    console.log('✅ Seed init.');
 
-    await seedTestEnvironment(prisma).catch((e) => {
-      console.error(e);
-      process.exit(1);
-    });
+    // Seeding test enviroment
+    execSync('npx tsx prisma/seed.ts');
+
+    console.log('✅ Seed finished.');
 
     return {
       async teardown() {
@@ -140,8 +38,6 @@ export default <Environment>{
             console.error('❌ Unkwown error:', err);
           }
         }
-
-        await prisma.$disconnect();
       },
     };
   },
